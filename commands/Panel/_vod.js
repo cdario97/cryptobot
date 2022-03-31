d4a26ed4ac9bbb2645c5fb093e461bfe786180c5
Samuel <89231917+SamuelDev00@users.noreply.github.com>
2022-03-31 19:57:45 -0400

    Add files via upload

---
+++ commands/Panel/_vod.js
@@ -0,0 +1,16 @@
+/*CMD
+  command: /vod
+  help: 
+  need_reply: true
+  auto_retry_time: 
+  folder: Panel
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+BBAdmin.installBot({
+  email: samuel.falconpc2@gmail.com,
+  bot_id: 607893
+})
+Bot.sendMessage("Bot was sent to "+mesaage)
